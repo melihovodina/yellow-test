572f41a1b06add530b555c08371782e4edc87262
melihovodina <melihoxa@gmail.com>
2024-11-11 12:55:07 +0300

    installed dependencies, connected to db, started users service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config";
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      dialectOptions: {
+        ssl: {
+          require: true
+        },
+      },
+      models: [],
+      autoLoadModels: true,
+    }),
+    UsersModule
+  ],
+  controllers: [],
+  providers: [],
 })
+
 export class AppModule {}
