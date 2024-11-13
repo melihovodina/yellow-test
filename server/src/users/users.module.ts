7300bb08342a5a093691626a81f90117d02ac78a
melihovodina <melihoxa@gmail.com>
2024-11-13 19:55:35 +0300

    deleted user controller

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,6 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import { AuthModule } from 'src/auth/auth.module';
@@ -8,7 +7,6 @@ import { Image } from 'src/images/models/image.model';
 import { Run } from 'src/run/models/run.model';
 
 @Module({
-  controllers: [UsersController],
   providers: [UsersService],
   imports: [
     SequelizeModule.forFeature([User, Run, Image]),
